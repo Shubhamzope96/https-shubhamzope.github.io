07b133bea4ada1258397f3c08cc54953b8cf8815
Shubhamzope96 <shubhamzope96@gmail.com>
2024-08-22 02:35:13 +0530

    P:pofile W:updated information and some UI bugs CR:Shubham

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faHome} from '@fortawesome/free-solid-svg-icons';
-import { faPerson } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 import { faContactCard } from '@fortawesome/free-solid-svg-icons';
 import { faHandsPraying} from '@fortawesome/free-solid-svg-icons';
 import { faList } from '@fortawesome/free-solid-svg-icons';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -18,13 +20,10 @@ export class HeaderComponent implements OnInit {
 
  
   constructor() { }
-
+   currEle:any;
   ngOnInit(): void {
-
-  }
-  showAlert(){
-    //this.("hbhsdgjfgj");
-    
+    debugger;
+    this.currEle=$("#home");
   }
   showNavOptions(){
     
@@ -35,11 +34,66 @@ export class HeaderComponent implements OnInit {
     document.getElementById('nav-icon')?.classList.add("show");
 
   }
+
+  activateAbout(){
+    debugger;
+    if(this.currEle!=document.getElementById('about-1')){
+      document.getElementsByClassName("active")[0].classList.remove("active");
+      debugger;
+      this.currEle.find("fa-icon").css("color","#FFF");
+      this.currEle.find("p").css("color","#FFF");
+      this.currEle.css("color","#FFF");
+      this.currEle=$("#about-1");
+      this.currEle.addClass("active");
+      $("#about-1").find("fa-icon").css("color","darkorange");
+      $("#about-1").find("p").css("color","darkorange");
+      
+    }
+  }
+  activateProjects(){
+    debugger;
+    if(this.currEle!=document.getElementById('projects-1')){
+      document.getElementsByClassName("active")[0].classList.remove("active");
+      this.currEle.find("fa-icon").css("color","#FFF");
+      this.currEle.find("p").css("color","#FFF");
+      this.currEle.css("color","#FFF");
+      this.currEle=$("#projects-1");
+      this.currEle.addClass("active");
+      $("#projects-1").find("fa-icon").css("color","darkorange");
+      $("#projects-1").find("p").css("color","darkorange");
+    }
+  }
+  activateConnect(){
+    debugger;
+    if(this.currEle!=document.getElementById('connect-1')){
+      document.getElementsByClassName("active")[0].classList.remove("active");
+      this.currEle.find("fa-icon").css("color","#FFF");
+      this.currEle.find("p").css("color","#FFF");
+      this.currEle.css("color","#FFF");
+      this.currEle=$("#connect-1");
+      this.currEle.addClass("active");
+      $("#connect-1").find("fa-icon").css("color","darkorange");
+      $("#connect-1").find("p").css("color","darkorange");
+    }
   
+  }
+  activateHome(){
+    debugger;
+    if(this.currEle!=document.getElementById('home')){
+      document.getElementsByClassName("active")[0].classList.remove("active");
+      this.currEle.find("fa-icon").css("color","#FFF");
+      this.currEle.find("p").css("color","#FFF");
+      this.currEle=$("#home");
+      this.currEle.addClass("active");
+      $("#home").css("color","darkorange");
+      $("#home").find("fa-icon").css("color","darkorange");
+    }
+  }
   home = faHome
-  person = faPerson
+  person = faUser
   projects = faLaptopCode
   contacts = faContactCard
   handsFolded = faHandsPraying
   navIcon = faList
+  connect = faUsers
 }
